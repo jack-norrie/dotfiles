80a36596b2e7753a48b40dc13fe20e44522075ef
Jack Norrie <jacknorrie6785@gmail.com>
2024-10-22 09:59:28 +0100

    Remove redudant nerdfont from wezterm.

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,6 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.font = wezterm.font("JetBrainsMono NF")
 config.font_size = 16
 
 config.enable_tab_bar = false
