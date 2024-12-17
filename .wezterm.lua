7015e45ce287924e52ff47e5862c757f498b72ae
jack-norrie <jacknorrie6785@gmail.com>
2024-12-17 15:18:01 +0000

    Remove opacity.
    Was buggy when running in virtual machines.

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,7 +12,7 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.9
+-- config.window_background_opacity = 0.9
 config.macos_window_background_blur = 20
 
 config.color_scheme = "Catppuccin Mocha"
